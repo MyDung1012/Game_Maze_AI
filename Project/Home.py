2752533c52a454161955870e8c2599823fc564bc
TranNg-0503 <nguyenphamnhattran@gmail.com>
2024-11-06 18:49:13 +0700

    first commit

--- Project/Home.py
+++ Project/Home.py
@@ -27,9 +27,9 @@ def draw_background():
 
 
 # Load slider images
-slider_image = pygame.image.load('Image/Slider.png')
+slider_image = pygame.image.load('Image/Slider.jpg')
 slider_image = pygame.transform.scale(slider_image, (280, 30))
-button_image = pygame.image.load('Image/Dragger.png')
+button_image = pygame.image.load('Image/Dragger.jpg')
 button_image = pygame.transform.scale(button_image, (30, 50))
 
 
@@ -45,11 +45,12 @@ def create_stars(num_stars):
         star = {
             'x': random.randint(0, screen_width),
             'y': random.randint(0, screen_height),
-            'size': random.randint(1, 3),
+            'size': random.randint(1, 2),
             'visible': True
         }
         stars.append(star)
     return stars
+
 
 # Draw background with stars
 def draw_background():
@@ -57,6 +58,7 @@ def draw_background():
     for star in stars:
         if star['visible']:
             pygame.draw.circle(screen, Colors.WHITE, (star['x'], star['y']), star['size'])
+
 
 # Draw colored text
 def draw_colored_text(surface, text, center):
@@ -108,7 +110,7 @@ custom_font = pygame.font.Font("Front/UTM-Birds-Paradise.ttf", 20)
 
 
 # Main variables
-stars = create_stars(100)  # Generate 100 stars
+#stars = create_stars(10000)  # Generate 1000 stars
 difficulty_value = 10  # Initial difficulty level
 
 # Main game loop
@@ -124,7 +126,7 @@ difficulty_value = 10  # Initial difficulty level
     value_text = font.render(f"{current_value}", True, Colors.YELLOW)
     surface.blit(value_text, (x + width + 20, y + (height // 2) - (value_text.get_height() // 2)))  # Xuất hiện bên phải thanh trượt
 """
-stars = create_stars(100)
+stars = create_stars(500)
 
 # Vòng lặp chính
 
@@ -141,11 +143,11 @@ while True:
 
 
             # Kiểm tra nếu click vào thanh trượt
-            if screen_width // 2 - 140 <= mouse_x <= screen_width // 2 + 140 and 280 <= mouse_y <= 310:
-                difficulty_value = ((mouse_x - (screen_width // 2 - 140)) // 28 + 1) * 10
+            if screen_width // 2 - 140 <= mouse_x <= screen_width // 2 + 140 and 580 <= mouse_y <= 610:
+                difficulty_value = min(100, max(10, ((mouse_x - (screen_width // 2 - 140)) // 28) * 10))
 
             # Kiểm tra nếu click vào nút "Start"
-            elif screen_width // 2 - 100 <= mouse_x <= screen_width // 2 + 100 and 350 <= mouse_y <= 410:
+            elif screen_width // 2 - 100 <= mouse_x <= screen_width // 2 + 100 and 650 <= mouse_y <= 710:
                 pygame.mixer.music.stop()
    
 
@@ -165,19 +167,37 @@ while True:
    # background_sound.play()
 
 
-    draw_colored_text(screen, "AI MAZE GAME", (screen_width // 2, 0))
+    #draw_colored_text(screen, "AI MAZE GAME", (screen_width // 2, 0))
 
-    custom_text = custom_font.render("MADE BY DUNG - DUONG - TRAN", True, Colors.PINK)
-    subtitle_rect = custom_text.get_rect(center=(screen_width // 2, 90))
-    screen.blit(custom_text, subtitle_rect)
+    # Tải hình ảnh logo
+    logo_image = pygame.image.load('Image/logo.png')
+    logo_image = pygame.transform.scale(logo_image, (600, 394))  # Thay đổi kích thước logo theo ý muốn
+
+    logo_x = (screen_width - logo_image.get_width()) // 2 # Dời logo xuống dưới một chút
+    logo_y = 40  # Điều chỉnh giá trị này để dời logo xuống (số càng lớn càng xuống dưới)
+    screen.blit(logo_image, (logo_x, logo_y))
+
+    # Thêm BY DUNG-DUONG-TRAN
+    additional_image = pygame.image.load('Image/by DDT.png')  # Thay đổi đường dẫn tới ảnh của bạn
+    additional_image = pygame.transform.scale(additional_image, (600, 50))  # Thay đổi kích thước ảnh theo ý muốn
+
+    # Đặt vị trí của ảnh dưới logo
+    additional_x = (screen_width - additional_image.get_width()) // 2
+    additional_y = logo_y + logo_image.get_height() + 5  # Đặt ảnh dưới logo với khoảng cách 20 pixel
+    screen.blit(additional_image, (additional_x, additional_y))
+
+
+    #custom_text = custom_font.render("MADE BY DUNG - DUONG - TRAN", True, Colors.PINK)
+    #subtitle_rect = custom_text.get_rect(center=(screen_width // 2, 90))
+    #screen.blit(custom_text, subtitle_rect)
 
     difficulty_font = pygame.font.SysFont("timesnewroman", 24)
     difficulty_label = difficulty_font.render(" SIZE: ", True, Colors.WHITE)
-    screen.blit(difficulty_label, (screen_width // 2 - 230, 280))
+    screen.blit(difficulty_label, (screen_width // 2 - 230, 580))
 
-    draw_difficulty_slider(screen, screen_width // 2 - 140, 280, 280, 30, 10, 100, difficulty_value)
+    draw_difficulty_slider(screen, screen_width // 2 - 140, 580, 280, 30, 10, 100, difficulty_value)
 
-    draw_rounded_button(screen, "START", screen_width // 2 - 100, 350, 200, 60, Colors.DARK_BLUE, 36)
+    draw_rounded_button(screen, "START", screen_width // 2 - 100, 650, 200, 60, Colors.DARK_BLUE, 36)
     draw_rounded_button(screen, "Exit", screen_width - 220, screen_height - 80, 200, 60, Colors.DARK_BLUE, 36)
 
 
